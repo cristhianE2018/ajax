827684c7e352884c3fa9af8b928ff9b2349b7a1c
cristhian <cristhyan_2011@hotmail.com>
2018-12-02 10:23:22 -0600

    Lista la primera parte del proyecto: mostrar alumnos

--- 15.- Proyecto_MostrarClientes/servidor.php
+++ 15.- Proyecto_MostrarClientes/servidor.php
@@ -1,5 +1,9 @@
 <?php
 require "conexion.php";
-
-
+$resultado = mysqli_query($con,"Select * from alumno");
+while($fila = mysqli_fetch_assoc($resultado)){
+	echo '<div>' . $fila['nombre']. '</div>';
+	echo '<input type = "hidden" value="'.$fila['sexo'].'">';
+}
+mysqli_close($con);
 ?>
