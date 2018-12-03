623d352591ed8768d06a5612d83446a403dc5242
Samuel Gómez Balderas <sierra_034@outlook.com>
2018-12-03 01:42:12 -0600

    Editar usuario

--- 19.- Proyecto2_CRUD/index.php
+++ 19.- Proyecto2_CRUD/index.php
@@ -34,6 +34,23 @@
 		}
 
 		mostrarUsuarios();
+
+		function editarUsuario(usuarioID) {
+			var nombreID = "nombreID" + usuarioID;
+			var emailID = "emailID" + usuarioID;
+			var borrar = "borrar" + usuarioID;
+			var actualizar = "actualizar" + usuarioID;
+			var editarNombreID = nombreID + "-editar";
+
+			var nombreDelUsuario = document.getElementById(nombreID).innerHTML;
+			var parent = document.querySelector("#" + nombreID);
+
+			if(parent.querySelector("#" + editarNombreID) === null) {
+				document.getElementById(nombreID).innerHTML = '<input type="text" id="' + editarNombreID +'" value="' + nombreDelUsuario +'">';
+				document.getElementById(borrar).disabled = "true";
+				document.getElementById(actualizar).style.display = "block";
+			}
+		}
 	</script>
 </body>
 </html>
--- 19.- Proyecto2_CRUD/servidor.php
+++ 19.- Proyecto2_CRUD/servidor.php
@@ -25,9 +25,9 @@ if($personas == "personas") {
 		$table .= '<td>' . $fila['ID_USUARIO'] . '</td>';
 		$table .= '<td id="' . $nombreID . $fila['ID_USUARIO'] . '">' . $fila['NOMBRE'] . '</td>';
 		$table .= '<td id="' . $emailID . $fila['ID_USUARIO'] . '">' . $fila['EMAIL'] . '</td>';
-		$table .= '<td><input type="text" value="Editar" class="btn btn-default"></td>';
-		$table .= '<td><input type="text" value="Eliminar" class="btn btn-danger"></td>';
-		$table .= '<td><input type="text" value="Actualizar" class="btn btn-primary" style="display: none;"></td>';
+		$table .= '<td><input id="' . $fila['ID_USUARIO'] . '" onclick="editarUsuario(this.id)" type="text" value="Editar" class="btn btn-default"></td>';
+		$table .= '<td><input id="' . $borrar . $fila['ID_USUARIO'] . '" type="text" value="Eliminar" class="btn btn-danger"></td>';
+		$table .= '<td><input id="' . $actualizar . $fila['ID_USUARIO'] . '" type="text" value="Actualizar" class="btn btn-primary" style="display: none;"></td>';
 		$table .= '</tr>';
 	}
 }
