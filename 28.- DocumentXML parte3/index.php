e2196425871380e4e5e4f238911e625f84a28694
cristhian <cristhyan_2011@hotmail.com>
2018-12-03 00:12:54 -0600

    Lista la practica 28

--- 28.- DocumentXML parte3/index.php
+++ 28.- DocumentXML parte3/index.php
@@ -4,34 +4,38 @@
 	<title> Leer XML </title>
 </head>
 <body>
-<button onclick="ejecutarAjax()"> Mostrar Informacion </button><br/><br/>
-<table id="info"></table>
-<script type="text/javascript">
-	function ejecutarAjax(){
-		var resultado = document.getElementById("info");
-		if(window.XMLHttpRequest){
-			xmlhttp = new XMLHttpRequest();
-		}else{
-			xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-		}
-		xmlhttp.onreadystatechange = function(){
-			if(xmlhttp.readyState===4 && xmlhttp.status === 200){
-					if(xmlhttp.responseXML !== null)
-					{
+	<select id="listaDeUsuarios" onchange="usuarioSeleccionado()"></select><br/><br/>
+	<div id="info"></div>
+	<script type="text/javascript">
+function ejecutarAjax(){
+			var resultado = document.getElementById("listaDeUsuarios");
+			if(window.XMLHttpRequest){
+				xmlhttp = new XMLHttpRequest();
+			}
+			else{
+				xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+			}
+			xmlhttp.onreadystatechange = function(){
+				if(xmlhttp.readyState === 4 && xmlhttp.status ===200){
+					if(xmlhttp.responseXML !== null){
 						var xmlDoc = xmlhttp.responseXML;
-						var tabla = "<tr><th>Nombre</th><th>Apellido</th></tr>";
+						var usuarios ="";
 						var persona = xmlDoc.getElementsByTagName("cliente");
-						for(var i =0; i<persona.length; i++){
-							tabla += "<tr><td>"+
-							persona[i].getElementsByTagName("nombre")[0].childNodes[0].nodeValue + "</td><td>" +
-							persona[i].getElementsByTagName("apellido")[0].childNodes[0].nodeValue + "</td>";
+						for(var i=0; i<persona.length; i++){
+							usuarios += "<option>" + persona[i].getElementsByTagName("nombre")[0].childNodes[0].nodeValue + "</option>";
 						}
-						resultado.innerHTML = tabla;
+						resultado.innerHTML = usuarios;
 					}
 				}
-	}
-	xmlhttp.open("GET","datos.xml",true);
-	xmlhttp.send();
+			}
+			xmlhttp.open("GET","datos.xml",true);
+			xmlhttp.send();
+		}
+		ejecutarAjax();
+function usuarioSeleccionado(){
+	var e = document.getElementById("listaDeUsuarios");
+	var usuario = e.options[e.selectedIndex].text;
+	document.getElementById("info").innerHTML = usuario + " fue seleccionado";
 }
 </script>
 </body>
